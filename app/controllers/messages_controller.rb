59727469cdb3c041b9fbcf7cfc5dc49644862d7c
高取もえ <aus.leon725@gmail.com>
2017-01-30 14:22:59 +0900

    set the new, create and private methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,17 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+    private
+  def message_params
+    params.require(:message).permit(:user_name, :format_id, :text, :image).merge(user_id: current_user.id)
+  end
 end
